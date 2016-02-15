fe939de6debf82bc92a89541248ce4bdf4613a6d
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2016-02-14 22:08:35 -0800

    added checking view

---
+++ builds/angular-registration/js/controllers/checkins.js
@@ -0,0 +1,4 @@
+myApp.controller('CheckInsController', ['$scope', '$rootScope', '$firebaseObject', '$firebaseArray', '$routeParams', 'FIREBASE_URL',
+ function($scope, $rootScope, $firebaseObject, $firebaseArray, $routeParams, FIREBASE_URL) {
+
+ }]);
