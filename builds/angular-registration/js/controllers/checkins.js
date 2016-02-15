cbda54c63ac4559a6727def52c6dcc7a1c68a6fa
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2016-02-14 22:24:43 -0800

    added checkin info into firebase database

--- builds/angular-registration/js/controllers/checkins.js
+++ builds/angular-registration/js/controllers/checkins.js
@@ -1,4 +1,22 @@
 myApp.controller('CheckInsController', ['$scope', '$rootScope', '$firebaseObject', '$firebaseArray', '$routeParams', 'FIREBASE_URL',
  function($scope, $rootScope, $firebaseObject, $firebaseArray, $routeParams, FIREBASE_URL) {
 
+   $scope.whichmeeting = $routeParams.mId;
+   $scope.whichuser = $routeParams.uId;
+
+   var ref = new Firebase(FIREBASE_URL + 'users/' + $scope.whichuser + '/meetings/' + $scope.whichmeeting + '/checkins');
+
+   $scope.addCheckin = function() {
+     var checkinsInfo = $firebaseArray(ref);
+     var myData = {
+       firstname : $scope.user.firstName,
+       lastname : $scope.user.lastName,
+       email : $scope.user.email,
+       date : Firebase.ServerValue.TIMESTAMP
+     };
+
+     checkinsInfo.$add(myData);
+
+   }
+
  }]);
