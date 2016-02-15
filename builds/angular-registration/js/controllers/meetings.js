95c10ba728b6e6d6ac5034ad366a01a22ee9679d
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2016-02-14 20:18:12 -0800

    added meeting controller

--- builds/angular-registration/js/controllers/meetings.js
+++ builds/angular-registration/js/controllers/meetings.js
@@ -1,3 +1,27 @@
-myApp.controller('MeetingsController', ['$scope', function($scope) {
+myApp.controller('MeetingsController', ['$scope', '$rootScope', '$firebaseAuth', '$firebaseArray', 'FIREBASE_URL',
+ function($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL) {
+
+   var ref = new Firebase(FIREBASE_URL);
+   var auth = $firebaseAuth(ref);
+
+   //on Auth
+   auth.$onAuth(function(authUser) {
+     //User Authenticated
+     if (authUser) {
+       var meetingsRef = new Firebase(FIREBASE_URL + 'users/' + $rootScope.currentUser.$id + '/meetings');
+       var meetingsInfo = $firebaseArray(meetingsRef);
+
+       //Add meetings
+       $scope.addMeeting = function() {
+         meetingsInfo.$add({
+           name: $scope.meetingname,
+           date: Firebase.ServerValue.TIMESTAMP
+           //promise
+         }).then(function() {
+           $scope.meetingname = '';
+         });
+       }
+     }
+   });
 
 }]);
